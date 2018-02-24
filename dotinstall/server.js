9c9674ba9d264a52722c5c7f3b4d803add3f30cf
佐藤利徳 <tyahhahha@gmail.com>
2018-02-24 23:55:59 +0900

    last study

--- dotinstall/server.js
+++ dotinstall/server.js
@@ -1,26 +1,42 @@
 const http = require('http'),
   fs = require('fs'),
-  ejs = require('ejs')
+  ejs = require('ejs'),
+  qs = require('querystring')
+
 const server = http.createServer()
 
 const settings = require('./settings.js')
 console.log(settings)
 
 const template =
-  fs.readFileSync(`${__dirname}/public/hello.ejs`, 'utf-8')
+  fs.readFileSync(`${__dirname}/public/bbs.ejs`, 'utf-8')
 
-let n = 0
+let posts = []
 
-server.on('request', (req, res) => {
-  n++
-  const data = ejs.render(template, {
-    title: 'hello',
-    content: 'world',
-    n: n,
-  })
+function renderForm(posts, res) {
+  const data = ejs.render(template, {posts})
   res.writeHead(200, {'Content-Type': 'text/html'})
   res.write(data)
   res.end()
+}
+
+server.on('request', (req, res) => {
+  if (req.method === 'POST') {
+    req.data = "";
+    req.on("readable", () => {
+      req.data += req.read()
+    })
+    req.on("end", () => {
+      const query = qs.parse(req.data)
+      console.log(req.data)
+      posts.push(query.name)
+      console.log(query)
+      renderForm(posts, res)
+    })
+  }
+  else {
+    renderForm(posts, res)
+  }
 })
 server.listen(settings.port, settings.host)
 console.log("server listening...")
